b84258ce452b9146cadb0736b813efe06b71a36f
Josh Bolduc <joshbolduc@users.noreply.github.com>
2021-12-02 23:51:21 -0500

    test: add StoryExplorerStoryFile tests
    Add tests to capture some salient data stored in StoryExplorerStoryFile.

---
+++ src/story/StoryExplorerStoryFile.test.ts
@@ -0,0 +1,46 @@
+import { readFileSync } from 'fs';
+import { basename, join } from 'path';
+import { sync } from 'fast-glob';
+import type { Uri } from 'vscode';
+import { testBaseDir } from '../../test/util/testBaseDir';
+import { parseStoriesFile } from '../parser/parseStoriesFile';
+import type { ParsedStoryWithFileUri } from '../parser/parseStoriesFileByUri';
+import { StoryExplorerStoryFile } from './StoryExplorerStoryFile';
+
+describe('StoryExplorerStoryFile', () => {
+  const tests = sync(['project/src/**/*.stories.*'], {
+    cwd: testBaseDir,
+  })
+    .map((file) => {
+      const storiesPath = join(testBaseDir, file);
+      const contents = readFileSync(storiesPath).toString();
+      const parsedRaw = parseStoriesFile(contents, basename(file));
+
+      return { file, parsedRaw };
+    })
+    .filter(({ parsedRaw }) => !!parsedRaw);
+
+  tests.forEach(({ file, parsedRaw }) => {
+    it(`parses story file ${file}`, () => {
+      const parsed: ParsedStoryWithFileUri = {
+        ...parsedRaw!,
+        file: {
+          path: `/mock/basedir/${file}`,
+        } as Uri,
+      };
+
+      const storyFile = new StoryExplorerStoryFile(parsed);
+
+      const stories = storyFile
+        .getAllStories()
+        .map(({ id, name, isDocs }) => ({ id, name, isDocs }));
+
+      expect({
+        id: storyFile.getId(),
+        title: storyFile.getTitle(),
+        isDocsOnly: storyFile.isDocsOnly(),
+        stories,
+      }).toMatchSnapshot();
+    });
+  });
+});
