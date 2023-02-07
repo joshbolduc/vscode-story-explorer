bac06ba2028a5450dfc7102aefc215822933510f
Josh Bolduc <joshbolduc@users.noreply.github.com>
2023-02-06 23:08:19 -0500

    test: rename "identifier as meta" test

--- test/project/v6/src/stories/TypeScriptIdentifierDefaultAsMeta.stories.ts
+++ test/project/v6/src/stories/TypeScriptIdentifierDefaultAsMeta.stories.ts
@@ -4,7 +4,7 @@ export const TypeScriptStory = () =>
   'Story for TS with exported identifier as Meta';
 
 const meta = {
-  title: 'Test/TypeScript/Identifier Exported as Meta',
+  title: 'Test/TypeScript/Identifier Default Exported as Meta',
 };
 
 export default meta as Meta;
