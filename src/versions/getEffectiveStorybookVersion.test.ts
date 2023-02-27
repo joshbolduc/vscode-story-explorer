4da0a28a023fc898bbaa5a85e6e92229a7151fa0
Josh Bolduc <joshbolduc@users.noreply.github.com>
2023-02-26 20:33:05 -0500

    refactor: add storybook version detection
    Add internal support for detecting the installed version of Storybook.
    
    This is based on:
    - a hidden editor setting
    - the detected version of `@storybook/cli` or `storybook` installed in
      the project
    - the detected version of `@storybook/cli` or `storybook` identified in
      a supported lockfile
    - the detected version (or range) of `@storybook/cli` or `storybook` in
      `package.json`
    
    If none of these approaches yield a version, a default version (~the
    most recent stable release) is used as a fallback. This is expected to
    be rare, since the heuristics above capture a range of scenarios
    (including those where a project's dependencies haven't been installed
    locally).
    
    Version detection allows the introduction of breaking behavioral changes
    without dropping support for older versions or requiring users to
    manually specify the version they use (or its corresponding behaviors).

---
+++ src/versions/getEffectiveStorybookVersion.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from 'vitest';
+import { getEffectiveStorybookVersion } from './getEffectiveStorybookVersion';
+
+describe('getEffectiveStorybookVersion', () => {
+  it('returns literal versions as-is', () => {
+    expect(getEffectiveStorybookVersion('6.5.0')).toBe('6.5.0');
+    expect(getEffectiveStorybookVersion('6.2.1')).toBe('6.2.1');
+    expect(getEffectiveStorybookVersion('1.2.3')).toBe('1.2.3');
+    expect(getEffectiveStorybookVersion('99.9.9-alpha.23')).toBe(
+      '99.9.9-alpha.23',
+    );
+  });
+
+  it('returns most compatible notable version when given range', () => {
+    expect(getEffectiveStorybookVersion('^6.0.0')).toBe('6.0.0');
+    expect(getEffectiveStorybookVersion('~6.2.0')).toBe('6.0.0');
+    expect(getEffectiveStorybookVersion('>= 4')).toBe('6.0.0');
+    expect(getEffectiveStorybookVersion('~6.0.0-alpha.30 <6.0.0')).toBe(
+      '6.0.0',
+    );
+    expect(getEffectiveStorybookVersion('*')).toBe('6.0.0');
+    expect(getEffectiveStorybookVersion('^7.0.0-alpha.0')).toBe('7.0.0');
+    expect(getEffectiveStorybookVersion('^7.0.0-alpha.53')).toBe('7.0.0');
+    expect(getEffectiveStorybookVersion('^7.0.0-beta.12')).toBe('7.0.0');
+    expect(getEffectiveStorybookVersion('^5.0.0')).toBe('6.0.0');
+    expect(getEffectiveStorybookVersion('^9.0.0')).toBe('6.0.0');
+  });
+});
---
+++ src/versions/versions.ts
@@ -0,0 +1,17 @@
+export const VERSION_6_x = '6.0.0';
+export const VERSION_7_x_ALPHA = '7.0.0-alpha.0';
+export const VERSION_7_x = '7.0.0';
+
+export const EARLIEST_VERSION = VERSION_6_x;
+
+// Versions are sorted according to preference when matching arbitrary ranges.
+// Unstable versions come last so that they are only matched if the given range
+// cannot be satisfied by a stable version (e.g., when the range's minimum
+// version is a beta release).
+
+export const notableVersions = [
+  // Stable versions, newest first
+  VERSION_6_x,
+  // Future/unstable version
+  VERSION_7_x,
+] as const;
